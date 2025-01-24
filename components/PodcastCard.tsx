8bea3855d9b0665492dcf3c42118f0872d633ca0
David Nyikes <90286144+Dawid930@users.noreply.github.com>
2025-01-24 15:11:07 +0100

    Enhance layout and functionality for PodWishperAI: - Updated Next.js configuration to allow TypeScript build errors and added remote image patterns. - Modified app metadata for title and description. - Implemented LeftSidebar, MobileNav, and RightSidebar components for improved navigation. - Created PodcastCard component to display podcast information dynamically. - Added sidebar links and podcast data constants for better structure and maintainability.

---
+++ components/PodcastCard.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const PodcastCard = ({
+  imgURL,
+  title,
+  description,
+  podcastId,
+}: {
+  imgURL: string
+  title: string
+  description: string
+  podcastId: number
+}) => {
+  return <div>PodcastCard</div>
+}
+
+export default PodcastCard

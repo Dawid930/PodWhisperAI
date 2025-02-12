e845e6547d713e8675ef763d991381e7269d5107
David Nyikes <90286144+Dawid930@users.noreply.github.com>
2025-02-12 17:41:36 +0100

    Add Clerk authentication and update Convex provider

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://endless-goat-92.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+}
+
+export default authConfig
